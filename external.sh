032ff52abde944c6499f33bae065c6f97b0c423f
rjain-iitr <rjain.iitr@gmail.com>
2017-01-31 15:16:52 +0530

    changes for server

--- external.sh
+++ external.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python dependency.py http://10.140.221.229:9000 external_dependency.json
+python dependency.py http://10.140.214.5:9000 external_dependency.json
 #python dependency.py "http://10.140.22.229:9000" external_dependency.json
 
